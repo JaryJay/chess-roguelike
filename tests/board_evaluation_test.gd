8e7e56066bf8e117311cb6d61ed03b73bac54d7f
Jay <jay.jrjren@gmail.com>
2024-10-11 11:41:24 -0400

    Create board_evaluation_test.gd

---
+++ tests/board_evaluation_test.gd
@@ -0,0 +1,9 @@
+
+
+static func test_1() -> void:
+	var board_state: = BoardState.new()
+	board_state.tiles[Vector2i(0, 0)] = Tile.new()
+
+static func add_tile_to_board_state(pos: Vector2i, state: BoardState) -> void:
+	var tile: = Tile.new()
+	tile.
