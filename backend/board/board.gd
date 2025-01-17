61d8d245e75eb2a083f242cd0b99848dd312924a
Jay <jay.jrjren@gmail.com>
2025-01-16 19:03:43 -0500

    Fix illegal move filtering

--- backend/board/board.gd
+++ backend/board/board.gd
@@ -15,37 +15,41 @@ func get_available_moves_from(from: Vector2i) -> Array[Move]:
 	assert(piece_map.has_piece(from), "Must be a piece there")
 	var piece: = piece_map.get_piece(from)
 	var moves: = piece.get_available_moves(self)
-	return filter_out_illegal_moves(moves)
+	moves = filter_out_illegal_moves_and_tag_check_moves(moves)
+	return moves
 
-func filter_out_illegal_moves(moves: Array[Move]) -> Array[Move]:
+func filter_out_illegal_moves_and_tag_check_moves(moves: Array[Move]) -> Array[Move]:
 	# Filter out all illegal moves
 	for i in range(moves.size() - 1, -1, -1):
 		var move: = moves[i]
 		#if move.is_castle():
 			#var castle_dir: = (move.to - move.from) / (move.to - move.from).abs().x
 		
-		var next_board: = perform_move(move)
+		var next_board: = perform_move(move, true)
 		if next_board.is_team_in_check(team_to_move):
 			# This is an illegal move
 			moves.remove_at(i)
 			continue
+		
+		# If the move puts the other team in check, tag it
 		if next_board.is_team_in_check(next_board.team_to_move):
 			# This move is a check
 			move.info = move.info | Move.CHECK
 		
-		
-	moves = moves.filter(
-		func(move: Move) -> bool:
-			var next_board: = perform_move(move)
-			return !next_board.is_team_in_check(next_board.team_to_move)
-	)
+	
+	# moves = moves.filter(
+	# 	func(move: Move) -> bool:
+	# 		var next_board: = perform_move(move)
+	# 		return !next_board.is_team_in_check(team_to_move)
+	# )
 	
 	# For castling moves specifically, we do some more checks:
 	# TODO
 	# moves = moves.filter(
 	return moves
 
-func perform_move(move: Move) -> Board:
+
+func perform_move(move: Move, allow_illegal: bool = false) -> Board:
 	var current_team_to_move: = team_to_move
 	
 	assert(piece_map.has_piece(move.from), "No piece at %s" % move.from)
@@ -70,7 +74,8 @@ func perform_move(move: Move) -> Board:
 		new_piece.pos = move.to
 		next_board.piece_map.put_piece(move.to, new_piece)
 	
-	#assert(!next_board.is_team_in_check(current_team_to_move), "A move cannot put your own team in check")
+	if !allow_illegal:
+		assert(!next_board.is_team_in_check(current_team_to_move), "A move cannot put your own team in check")
 	
 	return next_board
 
