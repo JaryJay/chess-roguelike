32e31084a07a39522570d267831cc23c095533a2
Jay <jay.jrjren@gmail.com>
2025-03-17 17:55:33 -0400

    feat: implement upgrade selection UI with upgrade generation and initialization

--- frontend/ui/upgrade_option.gd
+++ frontend/ui/upgrade_option.gd
@@ -4,5 +4,11 @@ signal chosen(upgrade: Upgrade)
 
 @export var upgrade: Upgrade
 
+func init(_upgrade: Upgrade) -> void:
+	assert(is_node_ready(), "UpgradeOption must be ready before initializing")
+	upgrade = _upgrade
+	%NameLabel.text = upgrade.display_name
+	%DescriptionLabel.text = upgrade.description
+
 func _on_choose_button_pressed() -> void:
 	chosen.emit(upgrade)
--- frontend/ui/upgrade_select_ui.gd
+++ frontend/ui/upgrade_select_ui.gd
@@ -1,11 +1,30 @@
-extends CanvasLayer
+class_name UpgradeSelectUI extends CanvasLayer
 
+signal upgrade_chosen(upgrade: Upgrade)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var upgrades_container: HBoxContainer = $Control/H
 
+func generate_upgrades(game_setup: GameSetup, num_upgrades: int = 3) -> void:
+	assert(is_node_ready(), "UpgradeSelectUI must be ready before generating upgrades")
+	for child: Node in upgrades_container.get_children():
+		child.queue_free()
+	
+	# Generate num_upgrades random upgrades, ensuring that they are unique and also applicable to the game setup
+	var applicable_upgrades: Array[Upgrade] = []
+	for upgrade in Upgrades.upgrades:
+		if upgrade.is_applicable(game_setup):
+			applicable_upgrades.append(upgrade)
+	# Hack to ensure that we have enough applicable upgrades
+	while applicable_upgrades.size() < num_upgrades:
+		applicable_upgrades.append(applicable_upgrades[randi() % applicable_upgrades.size()])
+	
+	applicable_upgrades.shuffle()
+	var upgrades: Array[Upgrade] = applicable_upgrades.slice(0, num_upgrades)
+	for upgrade in upgrades:
+		var upgrade_ui: UpgradeOption = load("res://frontend/ui/upgrade_option.tscn").instantiate()
+		upgrades_container.add_child(upgrade_ui)
+		upgrade_ui.init(upgrade)
+		upgrade_ui.chosen.connect(_on_upgrade_chosen.bind(upgrade))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_upgrade_chosen(upgrade: Upgrade) -> void:
+	upgrade_chosen.emit(upgrade)
