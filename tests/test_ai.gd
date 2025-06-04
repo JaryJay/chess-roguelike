71cc7d364abd733445a62bfe68a514f875fabfc0
Jay <jay.jrjren@gmail.com>
2025-06-04 11:49:50 -0400

    feat: update test ai to create board from string

--- tests/test_ai.gd
+++ tests/test_ai.gd
@@ -12,14 +12,6 @@ func _ready() -> void:
 	else:
 		run_tests()
 
-# Helper function to create a rectangular grid of tiles
-func create_tiles(top_left: Vector2i, bottom_right: Vector2i) -> Array[Vector2i]:
-	var tiles: Array[Vector2i] = []
-	for x in range(top_left.x, bottom_right.x + 1):
-		for y in range(top_left.y, bottom_right.y + 1):
-			tiles.append(Vector2i(x, y))
-	return tiles
-
 func run_tests() -> void:
 	Config.load_config()
 	PieceRules.load_pieces()
@@ -36,67 +28,68 @@ func run_tests() -> void:
 	get_tree().quit()
 
 func test_mate_in_one() -> void:
-	var b := Board.new()
+	var b := create_board_from_grid("""
+  .k..
+  ....
+  .K.R
+  ....
+""")
 	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,2), Vector2i(5,4)))
-	
-	# Black king trapped at bottom, White rook delivers mate from top, protected by pawn
-	b.piece_map.put_piece(Vector2i(3,2), Piece.new(Piece.Type.KING, Team.ENEMY_AI))  # Black king top left
-	b.piece_map.put_piece(Vector2i(3,4), Piece.new(Piece.Type.KING, Team.PLAYER))	# White king bottom left
-	b.piece_map.put_piece(Vector2i(5,4), Piece.new(Piece.Type.ROOK, Team.PLAYER))	# White rook bottom right
-	
 	var ai := _create_ai()
 	var move := ai.get_move(b)
 	
-	assert(move.from == Vector2i(5,4))  # Rook from bottom right
-	assert(move.to == Vector2i(5,2))	# Rook to top right, delivering mate
+	assert(move.from == Vector2i(5,2)) # Rook from bottom
+	assert(move.to == Vector2i(5,0)) # Rook to top, delivering mate
 	print("Mate in one passed")
 
 func test_obvious_queen_capture() -> void:
-	var b := Board.new()
+	var b := create_board_from_grid("""
+  K...
+  qR..
+  ..k.
+""")
 	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,2), Vector2i(5,5)))
-	
-	# Undefended queen can be captured by rook
-	b.piece_map.put_piece(Vector2i(2,2), Piece.new(Piece.Type.KING, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(4,4), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(2,3), Piece.new(Piece.Type.QUEEN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(3,3), Piece.new(Piece.Type.ROOK, Team.PLAYER))
 	
 	var ai := _create_ai()
 	var move := ai.get_move(b)
 	
-	assert(move.from == Vector2i(3,3) or move.from == Vector2i(2,2))
-	assert(move.to == Vector2i(2,3))
+	assert(move.from == Vector2i(3,2) or move.from == Vector2i(2,1))
+	assert(move.to == Vector2i(2,2))
 	assert(move.is_capture())
 	print("Obvious queen capture passed")
 
 func test_pawn_promotion_mate() -> void:
-	var b := Board.new()
-	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,4), Vector2i(5,7)))
-	
-	b.piece_map.put_piece(Vector2i(5,5), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(3,6), Piece.new(Piece.Type.KING, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(3,4), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
+	# The spaces are intentional to test that the AI can handle different kinds of boards
+	var b := create_board_from_grid("""
 
+
+
+  .k..
+  ...P.
+  .K.
+  ....
+""")
+	b.team_to_move = Team.PLAYER
 	var ai := _create_ai()
 	var move := ai.get_move(b)
 	
-	assert(move.from == Vector2i(5,5))  # Pawn from bottom left
-	assert(move.to == Vector2i(5,4))	# Pawn to top left, promoting
+	assert(move.from == Vector2i(5,5))  # Pawn
+	assert(move.to == Vector2i(5,4))	# Promote pawn
 	assert(move.promo_info == Piece.Type.QUEEN or move.promo_info == Piece.Type.ROOK)
 	print("Pawn promotion mate passed")
 
 func test_pawn_promotion_less_obvious() -> void:
-	var b := Board.new()
+	var b := create_board_from_grid("""
+
+
+
+  ....
+  .....
+  kp.K
+  ....
+""")
 	b.team_to_move = Team.ENEMY_AI
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,4), Vector2i(5,7)))
-
-	b.piece_map.put_piece(Vector2i(3,6), Piece.new(Piece.Type.PAWN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(2,6), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(4,6), Piece.new(Piece.Type.KING, Team.PLAYER))
-
+	
 	var ai := _create_ai()
 	var move := ai.get_move(b)
 
@@ -107,16 +100,14 @@ func test_pawn_promotion_less_obvious() -> void:
 	print("Pawn promotion less obvious passed")
 
 func test_knight_fork() -> void:
-	var b := Board.new()
+	var b := create_board_from_grid("""
+
+  K...
+  R...
+  N...
+  .q.k
+""")
 	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,2), Vector2i(5,5)))
-	
-	# Knight can fork enemy king and queen
-	b.piece_map.put_piece(Vector2i(2,2), Piece.new(Piece.Type.KING, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(2,3), Piece.new(Piece.Type.ROOK, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(2,4), Piece.new(Piece.Type.KNIGHT, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(5,5), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(3,5), Piece.new(Piece.Type.QUEEN, Team.ENEMY_AI))
 	
 	var ai := _create_ai()
 	var move := ai.get_move(b)
@@ -126,14 +117,15 @@ func test_knight_fork() -> void:
 	print("Knight fork passed")
 
 func test_obvious_pawn_capture() -> void:
-	var b := Board.new()
-	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,2), Vector2i(5,5)))
-	
-	b.piece_map.put_piece(Vector2i(2,4), Piece.new(Piece.Type.KING, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(4,2), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(3,4), Piece.new(Piece.Type.PAWN, Team.ENEMY_AI))
+	var b := create_board_from_grid("""
 
+  ..k.
+  ....
+  Kp..
+  ....
+""")
+	b.team_to_move = Team.PLAYER
+	
 	var ai := _create_ai()
 	var move := ai.get_move(b)
 
@@ -143,25 +135,16 @@ func test_obvious_pawn_capture() -> void:
 	print("Obvious pawn capture passed")
 
 func test_obvious_recapture() -> void:
-	var b := Board.new()
-	b.team_to_move = Team.PLAYER
-	b.tile_map.set_tiles(create_tiles(Vector2i(2,2), Vector2i(6,6)))
-	
-	b.piece_map.put_piece(Vector2i(3,2), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(4,2), Piece.new(Piece.Type.ROOK, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(2,3), Piece.new(Piece.Type.PAWN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(3,3), Piece.new(Piece.Type.PAWN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(4,3), Piece.new(Piece.Type.PAWN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(5,3), Piece.new(Piece.Type.QUEEN, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(3,4), Piece.new(Piece.Type.KNIGHT, Team.ENEMY_AI))
-	
-	b.piece_map.put_piece(Vector2i(2,5), Piece.new(Piece.Type.KNIGHT, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(3,5), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(5,5), Piece.new(Piece.Type.QUEEN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(6,5), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(4,6), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(6,6), Piece.new(Piece.Type.KING, Team.PLAYER))
+	var b := create_board_from_grid("""
 
+  .kr..
+  pppq.
+  .n...
+  NP.QP
+  ..P.K
+""")
+	b.team_to_move = Team.PLAYER
+	
 	# Make player queen capture enemy queen
 	b = b.perform_move(Move.new(Vector2i(5,5), Vector2i(5,3), Move.CAPTURE))
 	
@@ -174,55 +157,41 @@ func test_obvious_recapture() -> void:
 	print("Obvious recapture passed")
 
 # Player has forced checkmate, AI should react to it without breaking, even though it knows it's going to lose
-# Board: (lowercase letters are enemy pieces, uppercase letters are player pieces)
-#  ....
-# b...kq
-# ..QR..
-#  .P.P.
-#   .PK
 func test_react_to_forced_checkmate() -> void:
-	var b := Board.new()
+	var b := create_board_from_grid("""
+  ....
+ b...kq
+ ..QR..
+  .P.P.
+   .PK
+""")
 	b.team_to_move = Team.PLAYER
-	var tiles: Array[Vector2i] = create_tiles(Vector2i(1,0), Vector2i(4,3))
-	tiles.append_array([Vector2i(0,1), Vector2i(0,2), Vector2i(2,4), Vector2i(3,4), Vector2i(4,4), Vector2i(5,1), Vector2i(5,2), Vector2i(5,3)])
-	b.tile_map.set_tiles(tiles)
 	
-	b.piece_map.put_piece(Vector2i(0,1), Piece.new(Piece.Type.BISHOP, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(4,1), Piece.new(Piece.Type.KING, Team.ENEMY_AI))
-	b.piece_map.put_piece(Vector2i(5,1), Piece.new(Piece.Type.QUEEN, Team.ENEMY_AI))
-
-	b.piece_map.put_piece(Vector2i(2,2), Piece.new(Piece.Type.QUEEN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(3,2), Piece.new(Piece.Type.ROOK, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(2,3), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(4,3), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(3,4), Piece.new(Piece.Type.PAWN, Team.PLAYER))
-	b.piece_map.put_piece(Vector2i(4,4), Piece.new(Piece.Type.KING, Team.PLAYER))
-
 	# Make player queen give a check
-	b = b.perform_move(Move.new(Vector2i(2,2), Vector2i(1,1), Move.CHECK))
+	b = b.perform_move(Move.new(Vector2i(3,3), Vector2i(2,2), Move.CHECK))
 	assert(b.team_to_move == Team.ENEMY_AI)
 
 	var ai := _create_ai()
 	var ai_move1 := ai.get_move(b)
 	assert(ai_move1 != null, "AI should have moved the first time")
-	assert(ai_move1.from == Vector2i(4,1))
-	assert(ai_move1.to == Vector2i(4,0))
+	assert(ai_move1.from == Vector2i(5,2))
+	assert(ai_move1.to == Vector2i(5,1))
 
 	b = b.perform_move(ai_move1)
 	assert(b.team_to_move == Team.PLAYER)
 
-	b = b.perform_move(Move.new(Vector2i(1,1), Vector2i(2,0), Move.CHECK))
+	b = b.perform_move(Move.new(Vector2i(2,2), Vector2i(3,1), Move.CHECK))
 	assert(b.team_to_move == Team.ENEMY_AI)
 
 	var ai_move2 := ai.get_move(b)
 	assert(ai_move2 != null, "AI should have moved the second time")
-	assert(ai_move2.from == Vector2i(4,0))
-	assert(ai_move2.to == Vector2i(4,1))
+	assert(ai_move2.from == Vector2i(5,1))
+	assert(ai_move2.to == Vector2i(5,2))
 
 	b = b.perform_move(ai_move2)
 	assert(b.team_to_move == Team.PLAYER)
 
-	b = b.perform_move(Move.new(Vector2i(2,0), Vector2i(3,1), Move.CHECK))
+	b = b.perform_move(Move.new(Vector2i(3,1), Vector2i(4,1), Move.CHECK))
 	assert(b.is_match_over())
 	assert(b.get_match_result() == Match.Result.WIN)
 	
